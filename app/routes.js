782ad1c2eb439d95f8136422eccd18afb6e5a6a6
iamsinead <shdumigan@gmail.com>
2023-07-20 12:02:34 +0100

    it3 - migrate to v13

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const router = express.Router()
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
 
-const filters = require('./filters')(process.env)
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here - above the module.exports line
+// Add your routes here
+
 
 // all routes for registration v2
 router.use('/v2', require('./routes_v2')),
@@ -13,7 +17,8 @@ router.use('/v6', require('./routes_v6')),
 router.use('/v7', require('./routes_v7')),
 router.use('/v01-1', require('./routes_v01-1')),
 router.use('/v01-2', require('./routes_v01-2')),
-router.use('/v02-1', require('./routes_v02-1'))
+router.use('/v02-1', require('./routes_v02-1')),
+router.use('/v02-2', require('./routes_v02-2'))
 
 
 
@@ -44,5 +49,106 @@ router.use('/location', locationRouter)
 
 
 
+<<<<<<< Updated upstream
+=======
+router.post(['/locations-no-warnings/apply-filters', '/customise-settings/apply-filters'], (req, res) => {
+  if (req.session.data.clearFilters == "true") {
+    req.session.data.section = ""
+    req.session.data.metres = ""
+    req.session.data.role = ""
+    req.session.data.priority = ""
+    req.session.data.criticalityToBusiness = ""
+    req.session.data.filteredResults = ""
+    req.session.data.clearFilters = ""
+  } else if (req.session.data.continueLocations == "true" && req.originalUrl === '/locations-no-warnings/apply-filters') {
+    req.session.data.section = ""
+    req.session.data.metres = ""
+    req.session.data.role = ""
+    req.session.data.priority = ""
+    req.session.data.criticalityToBusiness = ""
+    req.session.data.filteredResults = ""
+    res.redirect('/v02-1/choosewarnings/flood-warnings-success');
+  } else if (req.session.data.continueSettings == "true" && req.originalUrl === '/customise-settings/apply-filters') {
+    req.session.data.section = ""
+    req.session.data.metres = ""
+    req.session.data.role = ""
+    req.session.data.priority = ""
+    req.session.data.criticalityToBusiness = ""
+    req.session.data.filteredResults = ""
+    res.redirect('/v02-1/choosewarnings/flood-warnings-set');
+  } else {
+    console.log('success test')
 
-module.exports = router
+  const allData = _.sortBy(data, 'sectionNumber')
+
+//filters
+let priorityFilter = req.session.data.priority;
+let criticalityToBusinessFilter = req.session.data.criticalityToBusiness;
+
+//set global scope of filtered results
+let filteredResults = [];
+
+//loop through each of the objects
+for (i of allData) {
+  // console.log(i.priority);
+  //if the object contains a matching value from the filter then add it to the filtered results array
+
+  if (typeof priorityFilter === 'undefined') {
+    priorityFilter= "";
+  }
+  if (i.priority.some((value) => priorityFilter.includes(value))) {
+    filteredResults.push(i);
+  }
+
+  if (typeof criticalityToBusinessFilter === 'undefined') {
+    criticalityToBusinessFilter= "";
+  }
+
+  console.log("i.criticalityToBusiness:", i.criticalityToBusiness);
+  console.log("criticalityToBusinessFilter:", criticalityToBusinessFilter);
+
+  if (Array.isArray(i.criticalityToBusiness) && i.criticalityToBusiness.some((value) => criticalityToBusinessFilter.includes(value))) {
+    filteredResults.push(i);
+  }
+}
+
+console.log("filteredResults:", filteredResults);
+
+
+
+//   let roleFilter = req.session.data.role
+//   if (typeof roleFilter === 'undefined') {
+//     roleFilter= ""
+//  }
+//  if (typeof roleFilter.length) {
+//     filteredResults = allData.filter(el => ( roleFilter.indexOf(el.role,el.role1,el.role2,el.role3) >= 0 ))
+//  }
+
+
+
+  req.session.data.filteredResults = filteredResults
+
+}
+  // get the URL of the page the user came from
+  const refererUrl = req.header('Referer');
+
+
+
+  // redirect the user back to the page they came from
+  res.redirect(refererUrl);
+})
+
+
+// set up route variable default status
+  router.get('/default', function (req, res) {
+	req.session.data = { route: 'default' }
+	res.redirect(`/v02-1/choosewarnings/overview-flood`)
+})
+
+// set up route variable customise status
+router.get('/customise', function (req, res) {
+	req.session.data = { route: 'customise' }
+	res.redirect(`/v02-1/choosewarnings/overview-flood`)
+})
+>>>>>>> Stashed changes
+
---
+++ app/routes_v02-2.js
@@ -0,0 +1,169 @@
+const express = require('express')
+const router = express.Router()
+
+var folder = "v02-2"
+var servicename = "Get flood warnings by phone, text or email"
+var paymentMethod = "govpay"  // or "govpay"
+
+
+// HTML for standard buttons
+var backlink = '<a href="javascript:history.back()" class="govuk-back-link">Back</a>'
+var submitButton = '<button type="submit" id="continueButton" class="govuk-button" name="Continue">Continue</button>'
+//var completeLink = '<a id="completeLink" href="/'+folder+'/save-and-return/check">Continue later</a>'
+var completeLink = ''
+
+function nocache(req, res, next) {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  next();
+}
+
+router.use(function (req, res, next) {
+    // set a folder and store in locals
+    // this can then be used in pages as {{folder}}
+    res.locals.folder=folder
+    res.locals.backlink=backlink
+    res.locals.submitButton=submitButton
+    res.locals.completeLink=completeLink
+    res.locals.paymentMethod=paymentMethod
+    res.locals.serviceName=servicename
+    // permit and autostore data set in all statement at bottom
+    res.locals.permit=res.locals.data
+    next()
+  });
+
+
+
+
+  // Run this code when a form is submitted to 'no-warning-answer'
+  router.post('/no-warning-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'no-warning'
+    var noWarning = req.session.data['no-warning']
+
+    // Check whether the variable matches a condition
+    if (noWarning == "increase"){
+      // Send user to next page
+      res.redirect('/v02-2/choosewarnings-customise/locations-no-warnings-found')
+    }
+
+
+     else {
+      // Send user to success page
+      res.redirect('/v02-2/choosewarnings-customise/flood-warnings-success')
+    }
+
+  })
+
+
+  // Run this code when a form is submitted to 'flood-warning-answer'
+  router.get('/flood-warning-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'flood-warning'
+    var floodWarning = req.session.data['flood-warning']
+
+    // Check whether the variable matches a condition
+    if (floodWarning == "customise"){
+      // Send user to next page
+      res.redirect('/v02-2/choosewarnings/customise-settings')
+    }
+
+
+     else {
+      // Send user to success page
+      res.redirect('/v02-2/choosewarnings/flood-warnings-set')
+    }
+
+  })
+
+
+
+
+  // Run this code when a form is submitted to 'flood-warning-answer'
+  router.get('/flood-warning-answer-customise', function (req, res) {
+
+    // Make a variable and give it the value from 'flood-warning'
+    var floodWarningCustomise = req.session.data['floodWarningCustomise']
+
+    // Check whether the variable matches a condition
+    if (floodWarningCustomise == "complex"){
+      // Send user to next page
+      res.redirect('/v02-2/choosewarnings/customise-settings')
+    }
+
+
+     else {
+      // Send user to success page
+      res.redirect('/v02-2/choosewarnings/warnings-set-success')
+    }
+
+  })
+
+  // Run this code when a form is submitted to 'flood-edit-answer'
+  router.post('/flood-edit-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'flood-edit'
+    var floodEdit = req.session.data['flood-edit']
+
+    // Check whether the variable matches a condition
+    if (floodEdit == "customise"){
+      // Send user to next page
+      res.redirect('/v02-2/choosewarnings-edit/customise-settings')
+    }
+
+
+     else {
+      // Send user to success page
+      res.redirect('/v02-2/choosewarnings-edit/are-you-sure')
+    }
+
+  })
+
+
+// customise or default user journey routing ==============================================================
+
+router.get('/choosewarnings/flood-options', function (req, res) {
+	res.render(folder + '/choosewarnings/flood-options',{
+		"formAction":"/"+ folder + "/choosewarnings/flood-options-routing"
+	})
+  })
+
+  router.post('/choosewarnings/flood-options', function (req, res) {
+	res.render(folder + '/choosewarnings/flood-options',{
+		"formAction":"/"+ folder + "/choosewarnings/flood-options-routing"
+	})
+  })
+
+  router.post('/choosewarnings-notinfwa/flood-options', function (req, res) {
+  res.render(folder + '/choosewarnings-notinfwa/flood-options',{
+    "formAction":"/"+ folder + "/choosewarnings-notinfwa/flood-options-routing"
+  })
+  })
+
+// Send permit data in session to every page ==================================
+router.all('*', function (req, res, next) {
+  res.locals.permit=res.locals.data
+  next()
+})
+
+  // Route to check if application has started and redirect
+  router.post('/choosewarnings/flood-options-routing', function (req, res) {
+	if (req.body['floodWarningCustomise']=="complex") {
+	  res.redirect("/"+ folder + "/flood-warning-answer-customise")
+	} else {
+	  res.redirect("/"+ folder + "/flood-warning-answer")
+	}
+  })
+
+  // Route to check if application has started and redirect
+  router.post('/choosewarnings-notinfwa/flood-options-routing', function (req, res) {
+	if (req.body['floodWarningCustomise']=="complex") {
+	  res.redirect("/"+ folder + "/flood-warning-answer-customise")
+	} else {
+	  res.redirect("/"+ folder + "/flood-warning-answer")
+	}
+  })
+
+
+module.exports = router
